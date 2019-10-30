58307d17b9aa2207c0c1953bd52a0eeb5d6a76a5
Alessandro Fiani <fiani.alessandro@gmail.com>
2019-10-30 10:18:29 +0100

    Edit requirements

--- ItsUmbria/Game/Abstractions/GameObject.cs
+++ ItsUmbria/Game/Abstractions/GameObject.cs
@@ -6,13 +6,24 @@ namespace ItsUmbria.Game.Abstractions
 {
     public abstract class GameObject
     {
+        //protected GameObject(string name) : this(name, Vector.Zero)
+        //{
+        //}
+
+        //protected GameObject(string name, Vector position)
+        //{
+        //    Name = name;
+        //    Position = position;
+        //}
+
         public string Type => this.GetType().Name;
-        public abstract string Name { get; }
+        public virtual string Name { get; }
         public Vector Position { get; set; } = new Vector(0, 0);
         public virtual void Print() => Console.WriteLine($"Object Name:{this.Name}");
     }
     public struct Vector
     {
+        public static Vector Zero = new Vector(0, 0);
         public Vector(double x, double y)
         {
             X = x;
