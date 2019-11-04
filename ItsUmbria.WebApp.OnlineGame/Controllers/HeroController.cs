ae516da5bc74fdc1bb6dae191e50d3df28e01645
Alessandro Fiani <fiani.alessandro@gmail.com>
2019-11-04 10:04:01 +0100

    Minor changes

--- ItsUmbria.WebApp.OnlineGame/Controllers/HeroController.cs
+++ ItsUmbria.WebApp.OnlineGame/Controllers/HeroController.cs
@@ -10,16 +10,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ItsUmbria.WebApp.OnlineGame.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class HeroController : GameController
     {
-        // Api/Hero/{teamColor}/{action}
-        [HttpPost("{teamColor}Team")]
+        // Api/Hero/{teamColor}
+        [HttpGet("{teamColor}")]
+        public IActionResult Status(TeamColor teamColor, HeroClass? heroClass, [FromQuery] string heroName)
+        {
+            Team team = Lobby.Instance.GetTeam(teamColor);
+            return new JsonResult(!string.IsNullOrEmpty(heroName)
+                ? heroClass != null ? team[heroClass.Value, heroName] : team[heroName]
+                : heroClass != null ? team[heroClass.Value] : (object) team);
+        }
+        [HttpPost("{teamColor}")]
         public IActionResult DoAction(TeamColor teamColor, [FromQuery] ActionType actionType, [FromQuery] string heroName, [FromQuery] string targetName, [FromQuery] int intensity, [FromQuery] double vectorX, [FromQuery] double vectorY) => new JsonResult(Lobby.Instance.GetTeam(teamColor)[heroName].DoAction(actionType, Lobby.Instance.GetHero(targetName), intensity, new Vector(vectorX, vectorY)));
-        [HttpPut("{teamColor}Team")]
+        [HttpPut("{teamColor}")]
         public IActionResult PickWeapon(TeamColor teamColor, [FromQuery] WeaponType weaponType, [FromQuery] string heroName) => new JsonResult(Lobby.Instance.GetTeam(teamColor)[heroName].PickWeapon(WeaponFactory.GetWeapon(weaponType)));
-        [HttpPatch("{teamColor}Team")]
+        [HttpPatch("{teamColor}")]
         public IActionResult DropWeapon(TeamColor teamColor, [FromQuery] WeaponType weaponType, [FromQuery] string heroName) => new JsonResult(Lobby.Instance.GetTeam(teamColor)[heroName].DropWeapon(weaponType));
+        // TODO: aggiungere azioni di cambio dell'arma
     }
 }
--- ItsUmbria.WebApp.OnlineGame/Controllers/TeamController.cs
+++ ItsUmbria.WebApp.OnlineGame/Controllers/TeamController.cs
@@ -15,14 +15,10 @@ namespace ItsUmbria.WebApp.OnlineGame.Controllers
     public class TeamController : GameController
     {
         // Api/Team{action}
-        [HttpGet("{teamColor=}/{heroClass=}")]
-        public IActionResult Status(TeamColor? teamColor, HeroClass? heroClass, [FromQuery] string heroName)
-        {
-            Team team = teamColor == null ? Lobby.Instance.CurrenTeam : Lobby.Instance.GetTeam(teamColor.Value);
-            return new JsonResult(!string.IsNullOrEmpty(heroName)
-                ? heroClass != null ? team[heroClass.Value, heroName] : team[heroName]
-                : heroClass != null ? team[heroClass.Value] : (object)team);
-        }
+        [HttpGet("{teamColor=}/")]
+        public IActionResult Status(TeamColor? teamColor) => new JsonResult(teamColor == null ? Lobby.Instance.CurrenTeam : Lobby.Instance.GetTeam(teamColor.Value));
+        [HttpGet("{teamColor=}/Members",Name = "Status")]
+        public IActionResult Members(TeamColor? teamColor) => new JsonResult((teamColor == null ? Lobby.Instance.CurrenTeam : Lobby.Instance.GetTeam(teamColor.Value)).Members);
         [HttpPut("{teamColor}")]
         public IActionResult Join(TeamColor teamColor, [FromQuery] HeroClass heroClass, [FromQuery] string heroName) => new JsonResult(Lobby.Instance.JoinTeam(HeroFactory.Create(heroClass, heroName), teamColor));
         [HttpDelete("{teamColor}")]
